7c31363a6a0c16c38e8a73c4d9727f4e9abd2fe3
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2025-08-29 00:34:42 +0530

    added function ot compare the incomming password with encrypted password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,6 @@
+const { JWT_KEY } = require("../config/serverConfig");
+const bcrypt = require("bcrypt");
+
 const UserRepository = require("../repository/user-repository");
 
 class UserService {
@@ -12,6 +15,31 @@ class UserService {
     } catch (error) {
       console.log("Something went wrong in service ;layer");
       throw { error };
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("omething went wrong in token creation");
+    }
+  }
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("SOmething went wrong in token validation", error);
+    }
+  }
+
+  checkPassword(userInputPlainPassword, encryptedPaasword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPaasword);
+    } catch (error) {
+      console.log("Something went wrong in password comparision");
     }
   }
 }
